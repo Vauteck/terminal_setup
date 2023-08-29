53e3a86ee37d917adb61a862f240d07c244f06f9
Quentin Aebischer <vauteck@hotmail.com>
2023-08-29 11:32:19 +0200

    First commit of install.sh file

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# prerequisites for building neovim
+sudo apt-get update
+sudo apt-get install -y ninja-build gettext cmake unzip curl
+sudo apt-get install -y tmux
+
+# neovim build
+git clone https://github.com/neovim/neovim.git
+cd neovim
+make CMAKE_BUILD_TYPE=RelWithDebInfo 
+sudo make install
+cd -
+
+# 
